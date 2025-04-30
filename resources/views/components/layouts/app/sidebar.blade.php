45599643e5626ba3140e883a4f9c4586c491b498
mohammadrahimi4662 <mohammadrahimi4662@gmail.com>
2025-04-30 10:57:05 +0330

    fixed error

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fa" dir="rtl" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
     </head>
